b4ca75eaf082ca922835e67d153b837868777cd5
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2016-10-26 08:49:45 +0200

    missing file

---
+++ src/Lahda/Lexer/Keywords.cs
@@ -0,0 +1,17 @@
+namespace Lahda.Lexer
+{
+    //if|for|while|var|int|float|string
+    public static class Keywords 
+    {
+        public const string IF = "if";
+        public const string TRUE = "true";
+        public const string FALSE = "false";
+        public const string ELSE = "else";
+        public const string FOR = "for";
+        public const string WHILE = "while";
+        public const string VAR = "var";
+        public const string INT = "int";
+        public const string FLOAT = "float";
+        public const string STRING = "string";
+    }
+}
