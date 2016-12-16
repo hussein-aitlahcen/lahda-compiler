052412cbdcfb182356821ba02ee812547a2df3e7
Hussein Ait-Lahcen <hussein.aitlahcen@gmail.com>
2016-12-16 13:08:19 +0100

    script helper

---
+++ testlib/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ..
+echo "#################"
+echo "#   COMPILING   #"
+echo "#################"
+dotnet run -p ./src/Lahda.Builder ./testlib/out.s ./testlib/$1
+echo "#################"
+echo "#   EXECUTING   #"
+echo "#################"
+./MSM/msm ./testlib/out.s
